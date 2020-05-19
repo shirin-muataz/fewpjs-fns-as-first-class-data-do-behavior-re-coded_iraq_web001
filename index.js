75ce9192b7d64b71fd258c43544669d34e4b7089
Shirin Muataz <shirinmuataz@gmail.com>
2020-05-19 22:32:35 +0000

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(sh)
+{
+  let y=parseInt(sh);
+  
+  if(y<12)
+  return("Good Morning");
+  if(y>=12 && y<=17)
+  return("Good Afternoon");
+  if(y>17 && y<=24)
+  return("Good Evening");
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(TEST)
+{
+ document.querySelector("#greeting").innerText=TEST;
+ 
+}
